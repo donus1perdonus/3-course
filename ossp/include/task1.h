6b4c63b57afabfb184e7a6de1de3afa4b64a6245
donus1perdonus <ekzozeta@gmail.com>
2025-11-13 20:15:56 +0300

    refactor

--- ossp/include/task1.h
+++
@@ -1,9 +0,0 @@
-#ifndef TASK1_H
-#define TASK1_H
-
-#include <stdio.h>
-
-void print_file_position(FILE *file);
-void task1_main(int argc, char *argv[]);
-
-#endif // TASK1_H
--- ossp/include/task6.h
+++
@@ -1,8 +0,0 @@
-#ifndef TASK6_H
-#define TASK6_H
-
-#include <stdio.h>
-
-void task6_main(void);
-
-#endif // TASK6_H
